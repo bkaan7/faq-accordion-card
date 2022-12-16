1cece12e4e271821cd82404272694e23dbed3def
Berkay Kaan Gedikli <112417568+bkaan7@users.noreply.github.com>
2022-12-16 17:47:38 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,54 +2,43 @@ $(document).ready(function () {
   // When the user clicks on a button
   $(".question1").click(function () {
     // Toggle the panel
-    $(".img1").toggleClass("up").toggleClass("rotate")
+    $(".img1").toggleClass("up").toggleClass("rotate");
     $(this).next(".panel").slideToggle();
-
-    
   });
 });
 
 $(document).ready(function () {
-    // When the user clicks on a button
-    $(".question2").click(function () {
-      // Toggle the panel
-      $(".img2").toggleClass("up").toggleClass("rotate")
-      $(this).next(".panel").slideToggle();
-  
-      
-    });
+  // When the user clicks on a button
+  $(".question2").click(function () {
+    // Toggle the panel
+    $(".img2").toggleClass("up").toggleClass("rotate");
+    $(this).next(".panel").slideToggle();
   });
-  
-  $(document).ready(function () {
-    // When the user clicks on a button
-    $(".question3").click(function () {
-      // Toggle the panel
-      $(".img3").toggleClass("up").toggleClass("rotate")
-      $(this).next(".panel").slideToggle();
-  
-      
-    });
-  });
+});
 
-  $(document).ready(function () {
-    // When the user clicks on a button
-    $(".question4").click(function () {
-      // Toggle the panel
-      $(".img4").toggleClass("up").toggleClass("rotate")
-      $(this).next(".panel").slideToggle();
-  
-      
-    });
+$(document).ready(function () {
+  // When the user clicks on a button
+  $(".question3").click(function () {
+    // Toggle the panel
+    $(".img3").toggleClass("up").toggleClass("rotate");
+    $(this).next(".panel").slideToggle();
   });
+});
 
-  $(document).ready(function () {
-    // When the user clicks on a button
-    $(".question5").click(function () {
-      // Toggle the panel
-      $(".img5").toggleClass("up").toggleClass("rotate")
-      $(this).next(".panel").slideToggle();
-  
-      
-    });
+$(document).ready(function () {
+  // When the user clicks on a button
+  $(".question4").click(function () {
+    // Toggle the panel
+    $(".img4").toggleClass("up").toggleClass("rotate");
+    $(this).next(".panel").slideToggle();
   });
-  
+});
+
+$(document).ready(function () {
+  // When the user clicks on a button
+  $(".question5").click(function () {
+    // Toggle the panel
+    $(".img5").toggleClass("up").toggleClass("rotate");
+    $(this).next(".panel").slideToggle();
+  });
+});
